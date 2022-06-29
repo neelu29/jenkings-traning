b4e2ab96d04b0f5c122d96b1875109823e81c5e4
neelu <abc@email.com>
2022-06-29 09:43:33 +0530

    commit successful

---
+++ src/traning/HelloAmdocs.java
@@ -0,0 +1,10 @@
+package traning;
+
+public class HelloAmdocs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("hello world!");
+	}
+
+}
